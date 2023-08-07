12098c0ac69520308c715d597bd5db2b6560b3b4
Dinçer Yiğit <dinceryigit60@gmail.com>
2023-08-07 15:11:39 +0300

    07.08

---
+++ src/demoProjects/rentACar/Domain/Entities/Model.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Persistence.Repositories;
+
+namespace Domain.Entities
+{
+    public class Model : Entity
+    {
+        public int BrandId { get; set; }
+        public int Id { get; }
+        public string Name { get; set; }
+        public decimal DailyPrice { get; set; }
+        public string ImageUrl { get; set; }
+        public virtual Brand? Brand { get; set; }  //virtual bir çok orm ile kullanılmasını sağlar
+
+        public Model()
+        {
+            
+        }
+        public Model(int id,int brandId,string name,decimal dailyPrice,string imageUrl): this()
+        {
+            Id = id;
+            BrandId = brandId;
+            Name = name;
+            ImageUrl = imageUrl;
+            Name = name;
+        }
+    }
+}
+  
--- src/demoProjects/rentACar/Persistence/Contexts/BaseDbContext.cs
+++ src/demoProjects/rentACar/Persistence/Contexts/BaseDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Domain.Entities;
+﻿using Core.Security.Entities;
+using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
@@ -8,6 +9,11 @@ namespace Persistence.Contexts
     {
         protected IConfiguration Configuration { get; set; }
         public DbSet<Brand> Brands { get; set; }
+        public DbSet<Model> Models { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<OperationClaim> OperationClaims { get; set; }
+        public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
 
         public BaseDbContext(DbContextOptions dbContextOptions, IConfiguration configuration) : base(dbContextOptions)
         {
@@ -21,10 +27,30 @@ namespace Persistence.Contexts
                 b.ToTable("Brands").HasKey(b=>b.Id);
                 b.Property(b => b.Id).HasColumnName("Id");
                 b.Property(b=>b.Name).HasColumnName("Name");
+                b.HasMany(b => b.Models);
+            });
+
+            modelBuilder.Entity<Model>(m =>
+            {
+                m.ToTable("Models").HasKey(b => b.Id);
+                m.Property(m=>m.Id).HasColumnName("Id");
+                m.Property(m => m.BrandId).HasColumnName("BrandId");
+                m.Property(m=>m.Name).HasColumnName("Name");
+                m.Property(m=>m.DailyPrice).HasColumnName("DailyPrice");
+                m.Property(m => m.ImageUrl).HasColumnName("ImageUrl");
+                m.HasOne(m => m.Brand);
             });
 
             Brand[] brandSeedData = { new(1, "Bmw"), new(2, "Mercedes") };
             modelBuilder.Entity<Brand>().HasData(brandSeedData);
+
+            Model[] modelEntitySeeds =
+            {
+                new(1, 1, "Series 4", 1500, ""),
+                new(2, 1, "Series 5", 1700, ""),
+                new(3, 2, "Series C", 2000, ""),
+            };
+            modelBuilder.Entity<Model>().HasData(modelEntitySeeds);
 
         }
     }
